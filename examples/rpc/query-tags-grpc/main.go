09914a3a166af5e5dae922e638868a54d32d3d1b
archi <aidamatrix@gmail.com>
2020-02-25 17:57:37 +0300

    Code illustrating missing tags in response to query via GRPC

---
+++ examples/rpc/query-tags-grpc/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/kubemq-io/kubemq-go"
+	"log"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	client, err := kubemq.NewClient(ctx,
+		kubemq.WithAddress("localhost", 50000),
+	//	kubemq.WithUri("http://localhost:9090"),
+		kubemq.WithClientId("test-query-client-id"),
+		kubemq.WithTransportType(kubemq.TransportTypeGRPC))
+	//	kubemq.WithTransportType(kubemq.TransportTypeRest))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	channel := "testing_query_channel"
+
+	go func() {
+		errCh := make(chan error)
+		queriesCh, err := client.SubscribeToQueries(ctx, channel, "", errCh)
+		if err != nil {
+
+			log.Fatal(err)
+		}
+		for {
+			select {
+			case err := <-errCh:
+				log.Fatal(err)
+				return
+			case query,more := <-queriesCh:
+				if !more {
+					fmt.Println("Query Received, done")
+					return
+				}
+				log.Printf("Query Received:\nId %s\nChannel: %s\nMetadata: %s\nBody: %s\n", query.Id, query.Channel, query.Metadata, query.Body)
+				err := client.NewResponse().
+					SetRequestId(query.Id).
+					SetResponseTo(query.ResponseTo).
+					SetExecutedAt(time.Now()).
+					SetMetadata("this is a response").
+					SetBody([]byte("got your query, you are good to go")).
+					SetTags(map[string]string{"key0":"val0","key1":"val1"}).
+					Send(ctx)
+				if err != nil {
+					log.Fatal(err)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+
+	}()
+	// give some time to connect a receiver
+	time.Sleep(1 *time.Second)
+	response, err := client.NewQuery().
+		SetId("some-query-id").
+		SetChannel(channel).
+		SetMetadata("some-metadata").
+		SetBody([]byte("hello kubemq - sending a query, please reply")).
+		SetTimeout(1 *time.Second).
+		Send(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Response Received:\nQueryID: %s\nExecutedAt:%s\nMetadat: %s\nBody: %s\n", response.QueryId, response.ExecutedAt, response.Metadata, response.Body)
+	fmt.Println("Response Tags Received:", response.Tags)
+}
